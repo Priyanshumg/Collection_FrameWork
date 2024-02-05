6c55a27c0446ac77df187c01b1826d2c21c831b5
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-05 10:22:55 +0530

    [Priyanshu] Add: Added file for stack

---
+++ Queue_Cases/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Queue_Cases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myq = new Queue();
+            myq.Enqueue("a");
+            myq.Enqueue("b");
+            myq.Enqueue("c");
+            foreach(object item in myq)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
